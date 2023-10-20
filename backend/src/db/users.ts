134d16a79c7cc7ff28be612052ce066da3306eda
tkny <zhtony0398@gmail.com>
2023-10-20 05:54:50 -0400

    user model

--- backend/src/db/users.ts
+++ backend/src/db/users.ts
@@ -10,3 +10,18 @@ const UserSchema = new mongoose.Schema({
     sessionToken: { type: String, select: false }
   }
 })
+
+export const UserModel = mongoose.model('User', UserSchema);
+
+// actions for controllers
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessoionToken = (sessionToken: string) => UserModel.findOne({
+  'authentication.sessionToken': sessionToken
+});
+export const getUsersById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values)
+  .save().then(user => user.toObject());
+
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import helmet from 'helmet';
+const morgan = require('morgan');
 const { database } = require('../config/database.ts');
 const { server } = require('../config/server');
 
@@ -12,9 +13,25 @@ app.use(cors({
   credentials: true
 }));
 
+app.use(morgan('dev'));
 app.use(compression());
 app.use(cookieParser());
-app.use(bodyParser.json());
+
+
+// sets headers to better secure app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
+
+// This middleware is provided by Express to parse incoming request bodies
+// with the x-www-form-urlencoded content type. The { extended: false }
+// option means it will use the querystring library to parse URL-encoded data.
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
 
 database();
 server();
