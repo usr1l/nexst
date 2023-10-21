213ab2e2489963e130aafa499fe01045c3e30fba
tkny <zhtony0398@gmail.com>
2023-10-20 20:01:23 -0400

    statc routes for react and xsrf

--- backend/config/index.ts
+++ backend/config/index.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  environment: process.env.NODE || 'development',
+  environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
   // dbFile: process.env.DB_FILE,
   dbUrl: process.env.MONGO_URL || ''
