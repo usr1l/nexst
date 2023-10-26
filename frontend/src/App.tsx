eb9a8ca80f9fbcfe98fa045ef42666ce572ac33c
tkny <zhtony0398@gmail.com>
2023-10-26 18:37:47 -0400

    app.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+const HomePage: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={''} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World</h1>
+        </header>
+      </div>
+    </>
+  )
+}
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
   );
 }
 
