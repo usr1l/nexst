0830dad30da6ad72ef5dfd041163be438d74648d
tkny <zhtony0398@gmail.com>
2023-10-26 18:39:39 -0400

    index.ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+const Root: React.FC = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  )
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
