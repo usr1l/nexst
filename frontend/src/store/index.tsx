8b6c5b4e389f2ac4735979acdb329c859e9f8c0a
tkny <zhtony0398@gmail.com>
2023-10-27 03:45:46 -0400

    folders

--- frontend/src/store/index.tsx
+++ frontend/src/store/index.tsx
@@ -1,1 +1,13 @@
+import { combineReducers } from "redux"
+
+// type store
+
+const rootReducer = combineReducers({
+
+});
+
+let enhancer;
+
+// if (process.env.NODE_ENV === 'production')
+
 export {}
