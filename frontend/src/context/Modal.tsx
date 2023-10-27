1f53b4ea0a646b531064f1a50877f3a283864089
tkny <zhtony0398@gmail.com>
2023-10-27 00:13:59 -0400

    modal fucntion

--- frontend/src/context/Modal.tsx
+++ frontend/src/context/Modal.tsx
@@ -1,20 +1,23 @@
 // frontend/src/context/Modal.js
 import React, { useRef, useState, useContext, RefObject } from 'react';
+import { ChildrenProviderProps } from '../interfaces';
 import ReactDOM from 'react-dom';
 import './Modal.css';
-import { ChildrenProviderProps } from '../interfaces';
 
-const ModalContext = React.createContext<ModalContextType | null>(null);
+// set it to an object and cast to modalcontexttype instead of using null in createcontext
+const ModalContext = React.createContext<ModalContextType>({} as ModalContextType);
 
 // set the type for modalcontext
 export type ModalContextType = {
   // don't need these nulls because ReactNode and HTMLElement already include null in their types
   // modalRef: RefObject<HTMLElement> | null;
   // modalContent: React.ReactNode | null;
-  modalRef: RefObject<HTMLElement> | null;
-  modalContent: React.ReactNode | null;
-  setModalContent: (content: React.ReactNode) => void | null;
-  setOnModalClose: (callback: () => void) => void | null;
+  modalRef: RefObject<HTMLElement>;
+  modalContent: React.ReactNode;
+
+  // setstate functions
+  setModalContent: React.Dispatch<React.SetStateAction<React.ReactNode>>;
+  setOnModalClose: React.Dispatch<React.SetStateAction<Function>>;
   closeModal: () => void | null;
 };
 
@@ -25,7 +28,7 @@ export function ModalProvider({ children }: ChildrenProviderProps): React.ReactN
 
   const [ modalContent, setModalContent ] = useState<React.ReactNode>(null);
   // callback function that will be called when modal is closing
-  const [ onModalClose, setOnModalClose ] = useState<(() => void) | null>(null);
+  const [ onModalClose, setOnModalClose ] = useState<Function | null>(null);
 
   const closeModal = () => {
     setModalContent(null); // clear the modal contents
@@ -50,25 +53,24 @@ export function ModalProvider({ children }: ChildrenProviderProps): React.ReactN
       {children}
       <div ref={modalRef}></div>
     </ModalContext.Provider>
+  );
+};
+
+export function Modal() {
+  // if (ModalContext === null) return (<>Error: no ModalContext</>)
+  const { modalRef, modalContent, closeModal } = useContext<ModalContextType>(ModalContext);
+  // If there is no div referenced by the modalRef or modalContent is not a
+  // truthy value, render nothing:
+  if (!modalRef || !modalRef.current || !modalContent) return null;
+
+  // Render the following component to the div referenced by the modalRef
+  return ReactDOM.createPortal(
+    <div id='modal'>
+      <div id='modal-background' onClick={closeModal} />
+      <div id='modal-content'>{modalContent}</div>
+    </div>,
+    modalRef.current
   )
 }
 
-// export function Modal() {
-//   const { modalRef, modalContent, closeModal } = useContext(ModalContext);
-//   // If there is no div referenced by the modalRef or modalContent is not a
-//   // truthy value, render nothing:
-//   if (!modalRef || !modalRef.current || !modalContent) return null;
-
-//   // Render the following component to the div referenced by the modalRef
-//   return ReactDOM.createPortal(
-//     <div id="modal" >
-//   <div id="modal-background" onClick = { closeModal } />
-//   <div id="modal-content" >
-//   { modalContent }
-//   < /div>
-//   < /div>,
-//     modalRef.current
-//   );
-// }
-
-// export const useModal = () => useContext(ModalContext);
+export const useModal = () => useContext(ModalContext);
