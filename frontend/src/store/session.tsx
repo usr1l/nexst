2a685d324b3194ea55cded7b81c4c7fa760a3c6f
tkny <zhtony0398@gmail.com>
2023-10-31 13:18:53 -0400

    create slice

--- frontend/src/store/session.tsx
+++ frontend/src/store/session.tsx
@@ -1,11 +1,5 @@
 import { Action } from "redux";
-import { createAction, createReducer, AnyAction, PayloadAction } from '@reduxjs/toolkit';
-
-const USER_LOGOUT = "USER_LOGOUT";
-const USER_LOGIN = "USER_LOGIN";
-
-const logout = createAction<null>(USER_LOGOUT);
-const login = createAction<UserState>(USER_LOGIN);
+import { createAction, createReducer, AnyAction, PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 export interface SessionStateType {
   isAuthenticated: boolean,
@@ -25,20 +19,41 @@ const initialState: SessionStateType = {
   user: {}
 };
 
-export default createReducer(initialState, (builder) => {
-  builder
-    .addCase(login, (state, action) => {
+export default createSlice({
+  name: 'session',
+  initialState: initialState,
+  reducers: {
+    logout: (state) => state = initialState,
+    login: (state, action) => {
       state.user = action.payload;
       state.isAuthenticated = true
-    })
-    .addCase(logout, (state, action) => {
-      state = initialState;
-    })
+    },
+  },
+  extraReducers: (builder) => {
+    // .addCase(someAction, (state, action) => {})
     // // use this for stricter type checking
     // .addMatcher(isActionWithNumberPayload, (state, action) => {})
-    .addDefaultCase((state, action) => state)
-});
+  }
+})
+
+
+// // create action types
+// const USER_LOGOUT = "USER_LOGOUT";
+// const USER_LOGIN = "USER_LOGIN";
+// const logout = createAction<undefined>(USER_LOGOUT);
+// const login = createAction<UserState>(USER_LOGIN);
 
 // function isActionWithNumberPayload(action: AnyAction): action is PayloadAction<number> {
 //   return typeof action.payload === 'number'
 // }
+// export default createReducer(initialState, (builder) => {
+//   builder
+//     .addCase(login, (state, action) => {
+//       state.user = action.payload;
+//       state.isAuthenticated = true
+//     })
+//     .addCase(logout, (state, action) => {
+//       state = initialState;
+//     })
+//     .addDefaultCase((state, action) => state)
+// });
